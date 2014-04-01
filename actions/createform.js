b85202fc8c94c8b6860d9d91accb4d969c78b69e
kk90 <kr.korycki@gmail.com>
2014-04-01 11:53:31 +0200

    :)

--- actions/createform.js
+++ actions/createform.js
@@ -3,37 +3,33 @@ var helpers = require("../helpers");
 var Policy = require("../s3post").Policy;
 var S3Form = require("../s3post").S3Form;
 var AWS_CONFIG_FILE = "config.json";
-var POLICY_FILE = "policy.json"; 
-//generowanie na podstawie pliku
-//skonstruwoanie pol formularza
-//zaszyfrowanie base64
+var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
+
 
 
 var task = function(request, callback){
 	//1. load configuration
-	var awsConfig=helpers.readJSONFile(AWS_CONFIG_FILE);
-	var policyData=helpers.readJSONFile(POLICY_FILE);
+	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
+	var policyData = helpers.readJSONFile(POLICY_FILE);
 
-	
 	//2. prepare policy
-	var policy =new Policy(policyData);
+	var policy = new Policy(policyData);
 
-
-	
 	//3. generate form fields for S3 POST
-	var s3Form= new S3Form(policy);
-	var fields= s3Form.generateS3FormFields();
+	var s3Form = new S3Form(policy);
+
+	var fields = s3Form.generateS3FormFields();
+
+	fields.concat(s3Form.generateS3CredientalsFields(awsConfig));
 	
 
-	fields.push()
-	
-	
 	//4. get bucket name
-	var bucket = policy.getConditionValueByKey("bucket");
+	var bucket = policy.getConditionValueByKey("bucket");	
 
-	
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:fields, bucket:bucket}});
+	callback(null, {template: INDEX_TEMPLATE, 
+		params:{fields:fields, bucket:bucket}
+	});
 }
 
 exports.action = task;
